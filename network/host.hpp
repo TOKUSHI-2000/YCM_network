e5462ae4a1247963342b5b678b8ae93a3af1323c
TOKUSHI-2000 <alpha.as.gsc@gmail.com>
2022-06-11 23:07:34 +0900

    通信システム(追加)

---
+++ network/host.hpp
@@ -0,0 +1,22 @@
+#ifndef FUNCTION_HOST_HPP
+#define FUNCTION_HOST_HPP
+
+#include "network.hpp"
+
+
+class Host : public communication
+{
+private:
+    int tmpNetHandle = -1;
+
+protected:
+
+
+public:
+    Host();
+
+    void IPcommu() override;
+    void setBuffer(netBufferDate date);
+};
+
+#endif
