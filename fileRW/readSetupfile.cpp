83b8e2a9ca9c0716f8ec18739405e00ebcee4602
TOKUSHI-2000 <alpha.as.gsc@gmail.com>
2022-06-30 00:19:18 +0900

    ファイルの読み込み処理の修理

--- fileRW/readSetupfile.cpp
+++ fileRW/readSetupfile.cpp
@@ -10,7 +10,7 @@ struct cfgDate
 
 int stringToInt(std::string str)
 {
-	std::cout << "値:" << str << "\n";
+	//std::cout << "\t値:" << str << "\n";
 	int tmpnNum = 0;
 
 	try
@@ -20,16 +20,17 @@ int stringToInt(std::string str)
 	}
 	catch(std::invalid_argument e)
 	{
-		ClearDrawScreen() ;
-		DrawString( 0, 0, (TCHAR*)"errer:\tIpアドレスが数字として認識できません", GetColor( 255, 255, 255), true);
-		ScreenFlip() ;
-		WaitKey();
+		//ClearDrawScreen() ;
+		std::cout <<"errer:\tIpアドレスが数字として認識できません\n";
+		//ScreenFlip() ;
+		//WaitKey();
 		
 		
 		exit(1);
 	} catch (std::out_of_range e) {
     return tmpnNum;
     }
+    return tmpnNum;
 }
 
 void readFile(IPDATA& ip, string& name, int& ModelId)
@@ -50,18 +51,18 @@ void readFile(IPDATA& ip, string& name, int& ModelId)
         std::cout << "ファイルを開けませんでした。\n" << std::endl;
         return;
     }
-
+	std::cout << "ファイルの読み込みに成功しました" << "\n";
 
     for (int i = 0; i <10; i++)
     {
-		if (!fp.eof())
+		if (fp.eof())
 		{
+			std::cout << "ファイルの読み込み終" << i <<"\n";
 			break;
 		}
 		
-        std::getline(fp, tmpStr[0]);
+        std::getline(fp, tmpStr[i]);
     }
-
 
 
 	for (int i = 0; i < 10; i++)
@@ -69,9 +70,14 @@ void readFile(IPDATA& ip, string& name, int& ModelId)
 		int a;
 		if (!tmpStr[i].empty())
 		{
-			a = tmpStr[i].find("=");
+			a = tmpStr[i].find(":");
+			if (a == std::string::npos)
+			{
+				continue;
+			}
+			
 			cdata[i].type = tmpStr[i].substr(0,a);
-			cdata[i].data = tmpStr[i].substr( a, tmpStr->length());
+			cdata[i].data = tmpStr[i].substr( a);
 		}
 
 		
@@ -79,36 +85,59 @@ void readFile(IPDATA& ip, string& name, int& ModelId)
 		
 	}
 	
+	std::cout << "データの分割完了\n";
+
 	for (int i = 0; i < 10; i++)
 		{
-			std::cout << "OK";
+			std::cout << "読み込みデータ:\n";
 			if (!tmpStr[i].empty())
 			{
+				std::cout << "\tデータあり\n";
 				if (cdata[i].type == "Ip")
 				{
 					string tmpCData;
 					string tmpnum;
                     int point;
 
-					tmpCData = cdata[i].type;
+					tmpCData = cdata[i].data;
+
+					for (int i = 0; i < tmpCData.length(); i++)
+					{
+						try
+						{
+							std::stoi(tmpCData.substr(0,1));
+						}
+						catch(std::invalid_argument e)
+						{
+							tmpCData.erase(0,1);
+							continue;
+						}
+						catch (std::out_of_range e)
+						{
+							break;
+    					}
+						break;
+					}
+
+
 					point = tmpCData.find(".");
 					tmpnum = tmpCData.substr( 0, point);
 					tmpCData.erase(0,point+1);
 					ip.d1 = stringToInt(tmpnum);
 					
-					tmpCData = cdata[i].type;
+					//tmpCData = cdata[i].data;
 					point = tmpCData.find(".");
 					tmpnum = tmpCData.substr( 0, point);
 					tmpCData.erase(0,point+1);
 					ip.d2 = stringToInt(tmpnum);
 
-					tmpCData = cdata[i].type;
+					//tmpCData = cdata[i].data;
 					point = tmpCData.find(".");
 					tmpnum = tmpCData.substr( 0, point);
 					tmpCData.erase(0,point+1);
 					ip.d3 = stringToInt(tmpnum);
 
-					tmpCData = cdata[i].type;
+					//tmpCData = cdata[i].data;
 					point = tmpCData.find(".");
 					tmpnum = tmpCData.substr( 0, point);
 					tmpCData.erase(0,point+1);
@@ -137,13 +166,12 @@ void readFile(IPDATA& ip, string& name, int& ModelId)
 
 
 			}
-			
-			std::cout << "\n";
 
 		}
 	
     
 
-	std::cout << "name:" << name << "\tIP:" << ip.d1 << "." << ip.d2 << "." << ip.d3 << "." << ip.d4; 
+	std::cout << "name:" << name << "\tIP:" << (int)ip.d1 << "." << (int)ip.d2 << "." << (int)ip.d3 << "." << (int)ip.d4; 
 
+	return;
 }
