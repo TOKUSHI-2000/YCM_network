faf711b33d91eec13cc220ad3685a2f11e726819
TOKUSHI-2000 <alpha.as.gsc@gmail.com>
2022-07-21 23:11:30 +0900

    不要ファイルの削除とco.batの掃除処理の追加

--- co.bat
+++ co.bat
@@ -5,3 +5,6 @@ g++ -c main.cpp avatar.cpp -DDX_GCC_COMPILE --exec-charset=cp932
 
 @rem g++ character.o main.o IPserver.o -Wl,-subsystem,windows -static -lDxLib -lDxUseCLib -lDxDrawFunc -ljpeg -lpng -lzlib -ltiff -ltheora_static -lvorbis_static -lvorbisfile_static -logg_static -lbulletdynamics -lbulletcollision -lbulletmath -lopusfile -lopus -lsilk_common -lcelt
 g++ main.o avatar.o -static -lDxLib -lDxUseCLib -lDxDrawFunc -ljpeg -lpng -lzlib -ltiff -ltheora_static -lvorbis_static -lvorbisfile_static -logg_static -lbulletdynamics -lbulletcollision -lbulletmath -lopusfile -lopus -lsilk_common -lcelt
+
+del main.o 
+del avatar.o
